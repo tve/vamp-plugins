c4f54951ea11fae11a0a0924975c09514a1edf20
Thorsten von Eicken <tve@voneicken.com>
2024-04-24 23:19:41 -0700

    force gzip archive compression

--- gen-package.sh
+++ gen-package.sh
@@ -17,6 +17,6 @@ make -C lotek clean
 cp -r DEBIAN $DESTDIR
 sed -e "/^Version/s/:.*/: $(date +%Y.%j)/" -i $DESTDIR/DEBIAN/control # set version: YYYY.DDD
 mkdir -p packages
-dpkg-deb --root-owner-group --build $DESTDIR packages
+dpkg-deb --root-owner-group -Zgzip --build $DESTDIR packages
 # dpkg-deb --contents packages
 ls -lh packages
